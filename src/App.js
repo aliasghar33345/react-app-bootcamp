b4ea7ec35871078fcd1d5139a4e338e218b7ec27
Muhammad Ali Asghar <50847199+aliasghar33345@users.noreply.github.com>
2020-06-24 16:21:22 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Dinner from './dinner';
+import Car from './cars.js';
 
 function App() {
   return (
     <div className="App">
-       <Dinner dishname="biryani"></Dinner>
+       <Car></Car>
     </div>
   );
 }
---
+++ src/cars.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Car(){
+    return(
+      <div>
+          <h1>My Car Name is Carolla</h1>
+      </div>
+    )
+}
+export default Car;
